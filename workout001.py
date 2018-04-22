c75a00ae888baae09143a6ca9f479515cd6933cf
tatohno <38090308+tatohno@users.noreply.github.com>
2018-04-22 16:57:45 +0900

    make wkot001

---
+++ workout001.py
@@ -0,0 +1,12 @@
+#!/usr.bin/python
+# coding UTF-8
+
+class Cat:
+    def __init__(self, color, tail):
+        self.color = color
+        self.tail = tail
+
+tora = Cat('orange', 'long')
+print(tora.color)
+
+
